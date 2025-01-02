e797992398295e8287c27964d8d625641645682d
ctholley <colt.holley@gmail.com>
2025-01-01 16:56:30 -0800

    adding some interview checks and testing run_all_checks() within the qaqc_script.qmd

--- R/interview_checks.R
+++ R/interview_checks.R
@@ -1,19 +1,124 @@
+# Missing trip status, all interviews should have a trip status of either "Incomplete" or "Complete"
 interview_missing.trip.status <- function(data) {
 
   interview <- data$interview
-  missing_count <- sum(is.na(interview$trip_status))
+  error_count <- sum(is.na(interview$trip_status), na.rm = TRUE)
 
-  if (missing_count > 0) {
-    return(tibble::tibble(
+  if (error_count > 0) {
+    return(create_results_table(
       pass = FALSE,
-      message = glue::glue("There are {missing_count} missing trip statuses in the interviews."),
-      critical = FALSE
+      critical = FALSE,
+      qaqc_check_type = "",
+      error_count = error_count,
+      message = glue::glue("There are {error_count} missing trip statuses in the interviews.")
     ))
   } else {
-    return(tibble::tibble(
+    return(create_results_table(
       pass = TRUE,
-      message = "All trip statuses are present in the interviews.",
-      critical = FALSE
+      critical = FALSE,
+      qaqc_check_type = "",
+      error_count = error_count,
+      message = "All trip statuses are present in all interviews."
+    ))
+  }
+}
+
+# Fishing end time > interview time
+interview_end.time.interview.time <- function(data) {
+
+  interview <- data$interview
+  error_count <- sum(interview$fishing_end_time > interview$interview_time, na.rm = TRUE)
+
+  if (error_count > 0) {
+    return(create_results_table(
+      pass = FALSE,
+      critical = FALSE,
+      qaqc_check_type = "",
+      error_count = error_count,
+      message = glue::glue("There are {error_count} interviews where the fishing end time is after the interview time.")
+    ))
+  } else {
+    return(create_results_table(
+      pass = TRUE,
+      critical = FALSE,
+      qaqc_check_type = "",
+      error_count = error_count,
+      message = "All fishing end times are before the interview time."
+    ))
+  }
+}
+
+# Fishing start time > fishing end time
+interview_start.time.end.time <- function(data) {
+
+  interview <- data$interview
+  error_count <- sum(interview$fishing_start_time > interview$fishing_end_time, na.rm = TRUE)
+
+  if (error_count > 0) {
+    return(create_results_table(
+      pass = FALSE,
+      critical = FALSE,
+      qaqc_check_type = "",
+      error_count = error_count,
+      message = glue::glue("There are {error_count} interviews where the fishing start time is after the fishing end time.")
+    ))
+  } else {
+    return(create_results_table(
+      pass = TRUE,
+      critical = FALSE,
+      qaqc_check_type = "",
+      error_count = error_count,
+      message = "All fishing start times are before the fishing end time."
+    ))
+  }
+}
+
+# Trailer Count > Vehicle Count
+interview_trailer.count.vehicle.count <- function(data) {
+
+  interview <- data$interview
+  error_count <- sum(interview$trailer_count > interview$vehicle_count, na.rm = TRUE)
+
+  if (error_count > 0) {
+    return(create_results_table(
+      pass = FALSE,
+      critical = FALSE,
+      qaqc_check_type = "",
+      error_count = error_count,
+      message = glue::glue("There are {error_count} interviews where the trailer count is greater than the vehicle count.")
+    ))
+  } else {
+    return(create_results_table(
+      pass = TRUE,
+      critical = FALSE,
+      qaqc_check_type = "",
+      error_count = error_count,
+      message = "All trailer counts are less than or equal to the vehicle count."
+    ))
+  }
+}
+
+# Angler Count > Group Count
+interview_angler.count.group.count <- function(data) {
+
+  interview <- data$interview
+  error_count <- sum(interview$angler_count > interview$total_group_count, na.rm = TRUE)
+
+  if (error_count > 0) {
+    return(create_results_table(
+      pass = FALSE,
+      critical = FALSE,
+      qaqc_check_type = "",
+      error_count = error_count,
+      message = glue::glue("There are {error_count} interviews where the angler count is greater than the group count.")
+    ))
+  } else {
+    return(create_results_table(
+      pass = TRUE,
+      critical = FALSE,
+      qaqc_check_type = "",
+      error_count = error_count,
+      message = "All angler counts are less than or equal to the group count."
     ))
   }
 }
--- R/run_all_checks.R
+++ R/run_all_checks.R
@@ -1,24 +1,35 @@
 #' run_all_checks
 #'
 #' @param data creel data
-#' @importFrom dplyr add_row mutate case_when
+#' @importFrom dplyr add_row mutate case_when bind_rows
 #' @returns results table
 #' @export
 run_all_checks <- function(data) {
+
+  # Initialize list to store results
+  results_list <- list()
+
   # List of check functions
   checks <- list(
-    interview_missing.trip.status = interview_missing.trip.status(data)
+    #interview checks
+    interview_missing.trip.status = interview_missing.trip.status,
+    interview_end.time.interview.time = interview_end.time.interview.time,
+    interview_start.time.end.time = interview_start.time.end.time,
+    interview_trailer.count.vehicle.count = interview_trailer.count.vehicle.count,
+    interview_angler.count.group.count = interview_angler.count.group.count
   )
 
-  # Loop through checks and fill the results tibble
+  # Loop through checks and store results in the results list
   for (check_name in names(checks)) {
-    check_result <- checks[[check_name]]
-    results <- add_row(results,
-                       check_name = check_name,
-                       pass = check_result$pass,
-                       message = check_result$message,
-                       critical = check_result$critical)
+    check_function <- checks[[check_name]]
+
+    # Call the function and store the result in the results list
+    check_result <- check_function(data)
+    results_list[[check_name]] <- check_result
   }
+
+  # Combine all check results into a single tibble using bind_rows
+  results <- bind_rows(results_list)
 
   # Add stoplight status colors
   results <- results |>
@@ -33,4 +44,29 @@ run_all_checks <- function(data) {
     )
 
   return(results)
+}
+
+#' create_results_table
+#'
+#' This helper function creates a results table for each QAQC check.
+#'
+#' @param pass logical / TRUE if the check passed, FALSE if it failed
+#' @param critical logical / TRUE if the check is critical, FALSE if it is not
+#' @param qaqc_check_type character / type of QAQC check
+#' @param error_count numeric / number of errors found for a given check
+#' @param message character / message to display in results table
+#' @returns template results table
+#' @importFrom tibble tibble
+#' @export
+create_results_table <- function(pass, critical, qaqc_check_type, error_count, message) {
+
+  table <- tibble::tibble(
+    pass = pass,
+    critical = critical,
+    qaqc_check_type = qaqc_check_type,
+    error_count = error_count,
+    message = message
+  )
+
+  return(table)
 }
