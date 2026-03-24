100534719c4623ba0a31d9621a72ae4a9474b7be
Colt Holley <colt.holley@dfw.wa.gov>
2026-03-24 16:36:58 -0700

    docs: update R/generate_report.R documentation to specify that VPN is required to output to mapped network drives like "T:/"

--- R/generate_report.R
+++ R/generate_report.R
@@ -3,10 +3,12 @@
 #' Renders a HTML report on data quality assurance/quality control (QAQC) for one or more freshwater creel fisheries
 #' and saves them to a specified directory.
 #'
+#' **Note - VPN Required:** By default reports are written to a mapped network "T:/" drive. You must be either directly connected to the internal network or through VPN for reports to save in this default location.
+#'
 #' @param fishery_names A character vector of freshwater creel fishery names.
 #' @param output_dir Path to the directory where rendered HTML reports will be
 #'   saved. Defaults to the standard WDFW Teams QAQC Reports folder. On Windows,
-#'   mapped drives are supported (e.g. "T:/path/to/folder").
+#'   mapped drives are supported (e.g., T:/path/).
 #'
 #' @return Invisibly returns a summary data frame of render results.
 #' @importFrom glue glue
